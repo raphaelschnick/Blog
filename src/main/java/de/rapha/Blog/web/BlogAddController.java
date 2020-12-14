47391b9d9a7c126fd2b8a33d675decab64e0627f
Schnick <rsc@sybit.de>
2020-12-14 15:27:31 +0100

    upload File

--- src/main/java/de/rapha/Blog/web/BlogAddController.java
+++ src/main/java/de/rapha/Blog/web/BlogAddController.java
@@ -2,6 +2,7 @@ package de.rapha.Blog.web;
 
 import de.rapha.Blog.blog.Blog;
 import de.rapha.Blog.blog.BlogService;
+import de.rapha.Blog.blog.FileUploadUtil;
 import de.rapha.Blog.category.Category;
 import de.rapha.Blog.category.CategoryService;
 import de.rapha.Blog.user.User;
@@ -9,10 +10,14 @@ import de.rapha.Blog.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
@@ -43,14 +48,22 @@ public class BlogAddController {
     }
 
     @PostMapping("/blog/add")
-    public String submit(@ModelAttribute("blog") Blog blog) {
+    public String submit(@ModelAttribute("blog") Blog blog,
+                         @RequestParam("image") MultipartFile multipartFile) throws IOException {
 
         User user = userService.get(2L);
 
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+        blog.setImage(fileName);
         blog.setDate(new Date());
         blog.setUser(user);
 
-        blogService.add(blog);
+        Blog result = blogService.add(blog);
+
+        String uploadDir = "blog/" + result.getId();
+
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 
         return "blog";
     }
