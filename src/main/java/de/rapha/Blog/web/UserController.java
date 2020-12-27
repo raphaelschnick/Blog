2fc1a054c9be5e31c05f6ccdebe6c20d890cd645
Raphael Schnick <raphael.schnick@sybit.de>
2020-12-27 20:18:36 +0100

    user page

--- src/main/java/de/rapha/Blog/web/UserController.java
+++ src/main/java/de/rapha/Blog/web/UserController.java
@@ -6,12 +6,18 @@ import de.rapha.Blog.category.Category;
 import de.rapha.Blog.category.CategoryService;
 import de.rapha.Blog.user.User;
 import de.rapha.Blog.user.UserService;
+import de.rapha.Blog.utils.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -38,5 +44,18 @@ public class UserController {
         model.addAttribute("categoryList", categoryList);
 
         return "user";
+    }
+
+    @PostMapping("/user")
+    public String submit(@ModelAttribute("user") User user,
+                         @RequestParam("image") MultipartFile multipartFile) throws IOException {
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+        String uploadDir = "data/user/" + user.getId();
+
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+        return "user" + user.getId();
     }
 }
